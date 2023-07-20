9aa7daed2c4791e1383445b186e7221aceeffc52
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-07-20 11:32:27 +0700

    Added: Card component

---
+++ resources/js/Components/Card.jsx
@@ -0,0 +1,25 @@
+const Card = ({ title, children, className = '' }) => {
+  return (
+    <div className="flex flex-col text-gray-500 rounded-lg border-2 shadow-xl overflow-hidden bg-white">
+        <div className={`p-4 text-2xl bg-gray-100/75 text-blue-600 outline outline-1 outline-gray-200` + className}>{title}</div>
+        {children}
+    </div>
+  )
+}
+
+const CardContent = ({ children, content, className = '' }) => {
+  return (
+    <>
+      <div className={`flex flex-col p-4` + className}>
+        <div className="flex items-center justify-between">
+          {content}
+        </div>
+        {children}
+      </div>
+    </>
+  )
+}
+
+Card.Content = CardContent
+
+export default Card
