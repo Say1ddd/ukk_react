1ef3c313878b56297e930b3863fd2c80600a56cf
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-10-20 11:47:50 +0700

    Added: factory for BarangMasuk & BarangKeluar model

---
+++ database/factories/BarangMasukFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BarangMasuk>
+ */
+class BarangMasukFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'tanggal_masuk' => now(),
+            'jumlah' => 1,
+            'barang_id' => rand(1, 10),
+        ];
+    }
+}
