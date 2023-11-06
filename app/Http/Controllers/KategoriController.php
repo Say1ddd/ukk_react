c2fdb4e1548397c45535552cefc016f7fcff1fb3
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-11-06 10:56:32 +0700

    Modify: Kategori model controller

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Kategori;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
+use App\Models\Kategori;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 
 class KategoriController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        $kategoris = Kategori::get();
+        $kategoris = Kategori::latest()->get();
 
         return Inertia::render('Kategori/Index', [
             'kategoris' => $kategoris
@@ -23,7 +29,7 @@ class KategoriController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): Response
     {
         return Inertia::render('Kategori/Create');
     }
@@ -31,23 +37,39 @@ class KategoriController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        $request->validate([
-            'slug' => 'required',
-            'deskripsi' => 'required',
-            'kategori' => 'required',
-        ]);
-
-        Kategori::create($request->all());
-
-        return redirect()->route('kategori.index');
+        DB::beginTransaction();
+        try {
+            $request->validate([
+                'deskripsi' => ['required', 'string', 'max:255'],
+                'kategori' => ['required', 'string', 'max:30'],
+            ], [
+                'required' => ':attribute harus diisi',
+                'max' => ':attribute maksimal :max karakter'
+            ]);
+    
+            $slug = Str::slug($request->deskripsi);
+    
+            Kategori::create([
+                'deskripsi' => $request->deskripsi,
+                'kategori' => $request->kategori,
+                'slug' => $slug
+            ]);
+    
+            DB::commit();
+            return Redirect::route('kategori.index')->with('success', 'Kategori berhasil ditambahkan');
+        } catch (\Exception $e) {
+            report($e);
+            DB::rollBack();
+            return Redirect::back()->with('error', 'Kategori gagal ditambahkan!');
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Kategori $kategori)
+    public function show(Kategori $kategori): Response
     {
         return Inertia::render('Kategori/Show', [
             'kategori' => $kategori
@@ -57,7 +79,7 @@ class KategoriController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Kategori $kategori)
+    public function edit(Kategori $kategori): Response
     {
         return Inertia::render('Kategori/Edit', [
             'kategori' => $kategori
@@ -67,26 +89,49 @@ class KategoriController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Kategori $kategori)
+    public function update(Request $request, Kategori $kategori): RedirectResponse
     {
-        $request->validate([
-            'slug' => 'required',
-            'deskripsi' => 'required',
-            'kategori' => 'required',
-        ]);
-
-        $kategori->update($request->all());
-
-        return redirect()->route('kategori.index');
+        DB::beginTransaction();
+        try {
+            $request->validate([
+                'deskripsi' => ['required', 'string', 'max:255'],
+                'kategori' => ['required', 'string', 'max:30'],
+            ], [
+                'required' => ':attribute harus diisi',
+                'max' => ':attribute maksimal :max karakter'
+            ]);
+    
+            $slug = Str::slug($request->deskripsi);
+    
+            $kategori->update([
+                'deskripsi' => $request->deskripsi,
+                'kategori' => $request->kategori,
+                'slug' => $slug
+            ]);
+    
+            DB::commit();
+            return Redirect::route('kategori.index')->with('success', 'Kategori berhasil diupdate');
+        } catch (\Exception $e) {
+            report($e);
+            DB::rollBack();
+            return Redirect::back()->with('error', 'Kategori gagal diupdate!');
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Kategori $kategori)
+    public function destroy(Kategori $kategori): RedirectResponse
     {
-        $kategori->delete();
-
-        return redirect()->route('kategori.index');
+        DB::beginTransaction();
+        try {
+            $kategori->delete();
+            DB::commit();
+            return Redirect::route('kategori.index')->with('success', 'Kategori berhasil dihapus');
+        } catch (\Exception $e) {
+            report($e);
+            DB::rollBack();
+            return Redirect::back()->with('error', 'Kategori gagal dihapus!');
+        }
     }
 }
