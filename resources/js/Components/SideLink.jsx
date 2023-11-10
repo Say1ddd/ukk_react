54866323276a09410085febeb190b538e4a1e9e6
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-11-10 10:57:41 +0700

    Modify: SideLink component styles

--- resources/js/Components/SideLink.jsx
+++ resources/js/Components/SideLink.jsx
@@ -6,10 +6,10 @@ const SideLink = ({ active = false, className = '', children, ...props }) => {
     <Link
     {...props}
     className={
-        'flex items-center text-md space-x-1 font-bold leading-5 ps-4 transition duration-150 py-3 ease-in-out focus:outline-none border-y border-transparent ' +
+        'flex items-center justify-start ps-4 text-sm space-x-2 font-bold leading-5 transition duration-150 py-4 ease-in-out focus:outline-none ' +
         (active
-            ? 'border-indigo-400 text-gray-900 bg-gray-200 focus:border-indigo-700'
-            : 'border-transparent text-gray-200 hover:bg-white/25 hover:border-gray-300 focus:text-white focus:border-gray-300 focus:bg-white/25') +
+            ? 'border-indigo-400 text-white focus:border-indigo-700'
+            : 'text-gray-100/65 hover:text-white focus:text-white focus:border-gray-300') +
         className
     }
 >
